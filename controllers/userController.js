7295707c630b0d8e084235f7f110ead459450e0e
Wendyydxiao <169132887+Wendyydxiao@users.noreply.github.com>
2024-10-04 22:07:39 +1000

    build controllers

---
+++ controllers/userController.js
@@ -0,0 +1,56 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  getUserById(req, res) {
+    User.findOne({ _id: req.params.id })
+      .populate('thoughts')
+      .populate('friends')
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  updateUser(req, res) {
+    User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.id })
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json({ message: 'User not found' });
+        }
+        return Thought.deleteMany({ _id: { $in: user.thoughts } });
+      })
+      .then(() => res.json({ message: 'User and thoughts deleted' }))
+      .catch((err) => res.status(500).json(err));
+  },
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendId } },
+      { new: true }
+    )
+    .then((user) => res.json(user))
+    .catch((err) => res.status(500).json(err));
+  },
+  removeFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId } },
+      { new: true }
+    )
+    .then((user) => res.json(user))
+    .catch((err) => res.status(500).json(err));
+  }
+};
